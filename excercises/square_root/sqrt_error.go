c8e6d81c11077e6be1a0bc0c3d3f074ac6d1d211
Surya <suryamailsyou@gmail.com>
2019-10-10 10:33:16 +0530

    Error fixed

--- excercises/square_root/sqrt_error.go
+++ excercises/square_root/sqrt_error.go
@@ -6,23 +6,24 @@ import (
 
 type ErrNegativeSqrt float64
 
-func (e ErrNegativeSqrt) Error() string{
-	return "caanot Sqrt negative number:"
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("can not Sqrt negative number:%v", float64(e))
 }
 
-func Sqrt(x ErrNegativeSqrt) ErrNegativeSqrt {
-	var z,before ErrNegativeSqrt = 1.0,2.0
-	for ; before!=z;{
-		before = z
-		z -= (z*z - x) / (2*z)
+func Sqrt(x float64) (float64, error) {
+	if x < 1 {
+		return 0, ErrNegativeSqrt(x)
 	}
-	return z
-
+	z := 1.0
+	before := 2.0
+	for before != z {
+		before = z
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
 }
-
 
 func main() {
-	var a,b ErrNegativeSqrt = 100.0,-4.0
-	fmt.Println(Sqrt(a))
-	fmt.Println(b)
+	fmt.Println(Sqrt(-100.0))
+	fmt.Println(Sqrt(100))
 }
